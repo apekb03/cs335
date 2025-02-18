8ad0052b541ad5518a4c218e0bd8185d8d10c1f7
apekb03 <apekshyabhattarai23@gmail.com>
2025-02-17 23:39:12 -0500

    all final

--- assignment3/dogs.js
+++ assignment3/dogs.js
@@ -1,16 +1,35 @@
-// const myButton=document.getElementById('breedInput');
-// myButton.oninput=function main() {
-//     fetch ('https://dog.ceo/dog-api/documentation/')
-//     .then(response=>response.json())
-//     .then(data=> {
-//         const datalist = document.getElementById("breedSuggestion");
-//         }
-//     )
+const photo=document.getElementById('photo')
+
+let nextImageTimer;
+function nextImage(){
+    clearTimeout(nextImageTimer);
+    search();
+}
+
+
+function search() {
+
+    const breedName=document.getElementById("search").value.replaceAll(" ","").toLowerCase();
+    fetch(`https://dog.ceo/api/breed/${breedName}/images/random`)
     
-// }
+    .then(response=>{
+        if(!response.ok){
+            throw new Error("Breed not found");
+        }
+            return response.json()})
+    .then(data=>{
+        const dogImage = document.getElementById("photo");
+        dogImage.src = data.message; // Set image source
+        
+        dogImage.style.display = "block"; // Show image
 
-
-fetch("https://dog.ceo/api/breeds/list/all")
-    .then(response=>response.json())
-    .catch(error=>console.error(error));
+        clearTimeout(nextImageTimer);
+        nextImageTimer=setTimeout(nextImage,5000);
+        photo.onclick=nextImage
+})
+    .catch(error => {
+        console.error(error);
+        alert("Breed not found!"); // Notify user
+});
+}
 
